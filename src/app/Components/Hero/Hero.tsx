e5400d919e22579665b567ef377b43e094f2681a
Ryle Urcia <ryleurcia@Ryles-Laptop.lan>
2023-12-31 08:45:56 -0500

    First commit

---
+++ src/app/Components/Hero/Hero.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import hero_bg from '@Public/assets/hero_img.jpg';
+import hero from './hero.module.css';
+
+export const Hero = () => {
+  return (
+    <div className={hero.hero_container}>
+      <div className={hero.heroImg_container}>
+        <Image src={hero_bg} alt="Hero" fill className={hero.bg_img} quality={90} priority={true}/>
+        {/*Content*/}
+      </div>
+      <div className={hero.content_container}>
+        <div className={hero.content_cta_container}>
+          <h1 className={hero.content_cta_heading}>Make your dream home <br/> <b>a reality</b>.</h1>
+          <button className={hero.content_cta_btn}>Get a quote</button>
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default Hero;
